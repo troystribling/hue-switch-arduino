2cbdf38d38423fcecfb5fdd32b1e0236b6f1911d
Troy Stribling <troy.stribling@gmail.com>
2014-03-09 15:28:21 -0700

    starting serial interface

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,5 +1,5 @@
 #include <SPI.h>
-#include <string.h>
+#include <EEPROM.h>
 #include "Adafruit_CC3000.h"
 #include "ccspi.h"
 #include "utils.h"
@@ -8,15 +8,56 @@
 
 HueLightsClient client(WEB_HOST, WEB_ROOT);
 
-void setup(void) {
+
+#ifdef SERIAL_INTERFACE
+
+#define MENU(X) Serial.println(F(X))
+
+uint8_t currentCommand = 0;
+
+void showMainMenu() {
+  MENU("\nHue Lights Main Menue");
+  MENU("1. Set Light On State");
+  MENU("2. Set All Lights On State");
+  MENU("3. Set Light Color");
+  MENU("4. Create Scene");
+  MENU("5. Set Scene");
+  MENU("6. Delete Scene");
+  MENU("7. Show Scenes");
+}
+
+void showSetLightOnMenu() {
+}
+
+void showSetAllLightsOnMenu() {
+}
+
+void showSetLightColorMenuw() {
+}
+
+void showCreateSceneMenue() {
+}
+
+void showSetSceneMenu() {
+}
+
+void showDeleteSceneMenu() {
+}
+
+#endif
+void setup() {
   Serial.begin(9600);
   DBUG_FREE_MEMORY;
   client.lanConnect(WLAN_SSID, WLAN_PASS);
-  if (client.lanDisconnect()) {
-    INFO_LOG(F("Disconnected from LAN"));
-  }
+#ifdef SERIAL_INTERFACE
+  showMainMenu();
+#endif
 }
 
-void loop(void) {
- delay(1000);
+void loop() {
+#ifdef SERIAL_INTERFACE
+  if (Serial.available()) {
+  }
+#else
+#endif
 }
